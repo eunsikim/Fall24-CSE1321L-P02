ffe3b3e9b79be7027008f35feb2b5e89ebddd3b6
eunsikim <kimeunsik229@gmail.com>
2024-08-21 19:43:14 -0400

    Week 2 ExampleB edit

--- Week-2/ExampleB.py
+++ Week-2/ExampleB.py
@@ -1,20 +1,19 @@
 def main():
-    number = int(
-        input("Enter a number: ")
-    )  # Take the input as a String and convert it to an integer
+    # Take the input as a String and convert it to an integer
+    number = int(input("Enter a number: "))
     anotherNumber = int(input("Enter another number: "))
 
+    # Addition +, Subtraction -, Multiplication *, division /
     operation = number / anotherNumber
 
-    print(
-        f"The division of {number} / {anotherNumber} is {round(operation, 3)}"
-    )  # round(number, decimal places)
+    # round(number, decimal places)
+    print(f"The division of {number} / {anotherNumber} is {round(operation, 3)}")
 
-    # 2 power of 3
+    # 2 power of 10
     power = pow(2, 10)  # pow(base, exponent)
-    power2 = 2**10
+    power2 = 2**10  # Same operation, different syntax
     print(f"POWER: {power}")
-    print(f"POWER: {power2}")  # Same operation, different syntax
+    print(f"POWER: {power2}")
 
     # Boolean operations
     # == equals, < less than, > greater than,
