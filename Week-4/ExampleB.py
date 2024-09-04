3d0c3962195ca3dc30990bb4b88468e92af2f1cd
eunsikim <kimeunsik229@gmail.com>
2024-09-04 19:28:38 -0400

    Week 4

--- Week-4/ExampleB.py
+++ Week-4/ExampleB.py
@@ -1,4 +1,10 @@
 def main():
+    device = "iphone"
+
+    if device == "Nokia" or device == "iphone":
+        print("PHONE")
+    elif device == "Dell" or device == "Asus":
+        print("COMPUTER")
 
 
 if __name__ == "__main__":
