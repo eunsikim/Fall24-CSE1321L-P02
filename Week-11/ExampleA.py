ef2be99c71d164862c9838eab1526f5035b13cb3
Kim Eun Sik <kimeunsik229@gmail.com>
2024-10-31 00:00:24 +0000

    Example on random.uniform() function to generate a random float number within a range

--- Week-11/ExampleA.py
+++ Week-11/ExampleA.py
@@ -1,3 +1,5 @@
+import random
+
 def multiplyByFive(x):
     return x * 5
 
@@ -15,6 +17,8 @@ def main():
     myList = list(map(multiplyByFive, myList))
 
     print(myList)
+
+    random.uniform(0, 1) #Generates a random float between 0 and 1
 
 if __name__ == "__main__":
     main()
